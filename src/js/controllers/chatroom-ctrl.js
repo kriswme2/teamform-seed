e28ad4d77e4475725d99e4ee115cbaad7b0d92db
Keith Hon <keithhon@Keiths-MacBook-Pro-2.local>
2016-11-13 17:08:10 +0800

    Added functionality to message box

--- src/js/controllers/chatroom-ctrl.js
+++ src/js/controllers/chatroom-ctrl.js
@@ -5,5 +5,13 @@ angular
   function($scope,$timeout,$firebaseArray,Chatroom) {
     var currentRoomRef = Chatroom.chroom("a");
     $scope.posts = $firebaseArray(currentRoomRef);
+
+    $scope.sendMessage = function(){
+            
+            Chatroom.send($scope.message);
+            $scope.message = "";
+
+    }
+
   }
 ]);
