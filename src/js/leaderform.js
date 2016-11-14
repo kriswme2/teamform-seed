494c2ed7b1ed4bd6d7830dccad5d5f2ba3654d98
felixchow95 <ms07b9@yahoo.com.hk>
2016-11-14 15:38:06 +0800

    leaderform tags function

--- src/js/leaderform.js
+++ src/js/leaderform.js
@@ -1,44 +1,46 @@
-var app = angular.module("Teams", ["firebase"]);
+var app = angular.module("Teams", ["firebase", 'ngTagsInput']);
 
-app.controller("TeamsCtrl", function ($scope, $firebaseObject, $firebaseArray) {
+app.controller("TeamsCtrl", function($scope, $firebaseObject, $firebaseArray) {
 
     var refPath = '';
     eventName = '-KWSQVV0KsVAY_bgeyo3'
 
     $scope.selector = {
         options: [],
-        selected: null
     }
 
     $scope.input = {
         teamName: '',
-        member: {}
+        teamSize: null,
+        tags: [],
+        member: []
     };
 
     initializeFirebase();
 
     refPath = 'Events/' + eventName;
-    retrieveOnceFirebase(firebase, refPath, function (data) {
+    retrieveOnceFirebase(firebase, refPath, function(data) {
         if (data.val() != null) {
             $scope.getEvent = data.val();
-            $scope.selector.selected = $scope.getEvent.minMem;
+            $scope.input.teamSize = $scope.getEvent.minMem;
             for (var i = $scope.getEvent.minMem; i <= $scope.getEvent.maxMem; i++)
                 $scope.selector.options.push(i);
         }
         $scope.$apply();
     });
 
-    $scope.addTeam = function () {
+    $scope.addTeam = function() {
 
         var newInput = {
-            'numOfMem': $scope.selector.selected,
+            'teamSize': $scope.input.teamSize,
             'regData': new Date().getTime(),
+            'tags': $scope.input.tags,
             'member': $scope.input.member
         };
 
         var teamPath = 'Events/' + eventName + '/Teams/' + $scope.input.teamName;
         var ref = firebase.database().ref(teamPath);
-        ref.set(newInput, function () { });
+        ref.set(newInput, function() { });
     }
 
 });
