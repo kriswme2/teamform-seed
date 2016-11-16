604d5e15f32b29c19c99dd331eb0a5d0608a8afe
ycliae <ycliae@connect.ust.hk>
2016-11-16 14:53:37 +0800

    deleted unused func list() from NotificationCtrl

--- src/js/controllers/notification-ctrl.js
+++ src/js/controllers/notification-ctrl.js
@@ -6,14 +6,5 @@ angular
     $scope.notifications = Notification.list("bb");
 
     //Notification.send("aa", "bb", "4434");
-
-
-    $scope.list = function() {
-      $scope.noww = Notification.list().length;
-      // $timeout(function(){
-      //   $scope.list();
-      // },500);
-    };
-    //$scope.list();
   }
 ]);
