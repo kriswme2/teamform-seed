5f431ee3281be7ef0660e2c30ffaae7ba83d745a
Kris <mcwongag@ust.hk>
2016-11-27 02:09:37 +0800

    Added Firebase App Server

---
+++ server/app.js
@@ -0,0 +1,23 @@
+var admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert("certs/lab-firebase-aefba-firebase-adminsdk-0n4ed-ff41738641.json"),
+  databaseURL: "https://lab-firebase-aefba.firebaseio.com"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+
+// Teams
+var teams_ref = db.ref("teams");
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+// Events
+var teams_ref = db.ref("teams");
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+/* Instead of doing console.log to display the entire database, it can do notification, send email! */
