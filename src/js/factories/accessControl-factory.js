a280d4c6eee68bd2f074f97008ebad5d36c63979
ycliae <ycliae@connect.ust.hk>
2016-11-27 10:08:49 +0800

    test for modifing to serve public events.

--- src/js/factories/accessControl-factory.js
+++ src/js/factories/accessControl-factory.js
@@ -1,17 +1,19 @@
 angular
   .module('teamform')
-  .factory("AccessControl", ["firebase", "$firebaseArray", "Auth", "$q",function(firebase,$firebaseArray,Auth,$q) {
+  .factory('AccessControl', ['firebase', '$firebaseArray', 'Auth', 'Events', '$q',function(firebase,$firebaseArray,Auth,Events,$q) {
     var val = {};
     val.eventID = null;
     val.access = null;
-    var ref = firebase.database().ref("accessControls");
+    var ref = firebase.database().ref('accessControls');
     var currentUserRef = null;
+    var eventRef = null;
     var deferred = null;
 
     var AccessControl = {
-      setcurrentUser: function (eventID) {
+      init: function (eventID) {
         val.eventID = eventID;
         currentUserRef = ref.child(val.eventID).child(Auth.$getAuth().uid);
+        eventRef = Events.ref();
       },
       get: function(){
         return currentUserRef;
@@ -22,12 +24,22 @@ angular
       requireAccess: function(eventID) {
         if (deferred) deferred.reject('reset promise');
         deferred = $q.defer();
-        AccessControl.setcurrentUser(eventID);
-        currentUserRef.on("value", function(snapshot){
-          if (snapshot.val()) {
-            val.access = snapshot.val().access;
+        AccessControl.init(eventID);
+        eventRef.orderByKey().equalTo(eventID).on('value', function(snapshot){console.log(snapshot.val()[eventID].privacy);
+          if (!snapshot) {console.log('reject');
+            deferred.reject('no this event');
+          } else if (snapshot.val()[eventID].privacy == 'public') {
+            deferred.resolve();
+          } else {
+            currentUserRef.on('value', function(snapshot){
+              if (snapshot.val()) {
+                val.access = snapshot.val().access;
+              }
+              deferred.resolve();
+            }, function(){
+              deferred.reject('firebase error');
+            });
           }
-          deferred.resolve();
         }, function(){
           deferred.reject('firebase error');
         });
