b79b9d4fdda7551d26f67c2c44445c4830da4abe
Felix Chow <cwchowab@connect.ust.hk>
2016-11-28 23:30:51 +0800

    event search tags

--- src/js/controllers/skillsMatch-ctrl.js
+++ src/js/controllers/skillsMatch-ctrl.js
@@ -12,34 +12,36 @@ function SkillsMatchCtrl($scope, $firebaseArray, Events, Teams, Auth, $statePara
     $scope.eSearch = [];
     $scope.tSearch = [];
 
+    $scope.uResult = [];
+    $scope.eResult = [];
+    $scope.tResult = [];
+
     $scope.uTags = $firebaseArray(Tags.uref);
     $scope.eTags = $firebaseArray(Tags.eref);
     $scope.tTags = $firebaseArray(Tags.tref);
 
-    // $scope.arr = [];
-
-    $scope.SearchUser = function () {
+    $scope.SearchUser = function() {
         if ($scope.uSearch !== []) {
-            $scope.uTags.$loaded().then(function (users) {
-                angular.forEach(users, function (user) {
+            $scope.uResult = [];
+            $scope.uTags.$loaded().then(function(users) {
+                angular.forEach(users, function(user) {
                     if (user.tags !== null) {
                         var uId = user.$id;
-                        // $scope.arr = user.tags;
 
                         var sCount = Object.keys($scope.uSearch).length;
                         var uCount = Object.keys(user.tags).length;
                         if (sCount <= uCount) {
                             var found = 0;
 
-                            $scope.uSearch.some(function (sTag) {
-                                 user.tags.some(function (uTag) {
+                            $scope.uSearch.some(function(sTag) {
+                                user.tags.some(function(uTag) {
                                     if ($filter('lowercase')(sTag.text) === $filter('lowercase')(uTag.text))
                                         found++;
                                 });
                             });
 
                             if (found !== 0) {
-                                firebase.database().ref('users').child(uId).once("value").then(function (data) {
+                                firebase.database().ref('users').child(uId).once("value").then(function(data) {
                                     if (data.val() !== null) {
                                         var Match = {
                                             email: data.val().email,
@@ -57,28 +59,28 @@ function SkillsMatchCtrl($scope, $firebaseArray, Events, Teams, Auth, $statePara
         }
     };
 
-    $scope.SearchEvent = function () {
+    $scope.SearchEvent = function() {
         if ($scope.eSearch !== []) {
-            $scope.eTags.$loaded().then(function (eData) {
-                angular.forEach(eData, function (event) {
+            $scope.eResult = [];
+            $scope.eTags.$loaded().then(function(eData) {
+                angular.forEach(eData, function(event) {
                     if (event.tags !== null) {
                         var eId = event.$id;
-                        // $scope.arr = user.tags;
 
                         var sCount = Object.keys($scope.eSearch).length;
                         var eCount = Object.keys(event.tags).length;
                         if (sCount <= eCount) {
                             var found = 0;
 
-                            $scope.eSearch.some(function (sTag) {
-                                 event.tags.some(function (eTag) {
-                                    if (sTag.text === eTag.text)
+                            $scope.eSearch.some(function(sTag) {
+                                event.tags.some(function(eTag) {
+                                    if ($filter('lowercase')(sTag.text) === $filter('lowercase')(eTag.text))
                                         found++;
                                 });
                             });
 
                             if (found !== 0) {
-                                Events.childRef(eId).once("value").then(function (data) {
+                                Events.childRef(eId).once("value").then(function(data) {
                                     if (data.val() !== null) {
                                         var Match = {
                                             title: data.val().title,
@@ -95,9 +97,14 @@ function SkillsMatchCtrl($scope, $firebaseArray, Events, Teams, Auth, $statePara
         }
     };
 
-    $scope.uResult = [];
-    $scope.eResult = [];
-    // $scope.tResult = [];
-
-
+    // $scope.SearchTeam = function() {
+    //     if ($scope.tSearch !== []) {
+    //         $scope.tResult = [];
+    //         $scope.tTags.$loaded().then(function(tData) {
+    //             angular.forEach(tData, function(team) {
+    //               if()
+    //             });
+    //         });
+    //     }
+    // }
 }
