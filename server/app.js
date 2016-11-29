33cbc51e11164255f8d5785ba5e0c6326e2d379e
Kris <mcwongag@ust.hk>
2016-11-29 14:52:39 +0800

    firebase cloud messaging

--- server/app.js
+++ server/app.js
@@ -8,16 +8,24 @@ admin.initializeApp({
 // As an admin, the app has access to read and write all data, regardless of Security Rules
 var db = admin.database();
 
-// Teams
-var teams_ref = db.ref("teams");
-teams_ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
-});
+// // Teams
+// var teams_ref = db.ref("teams");
+// teams_ref.on("value", function(snapshot) {
+//   console.log(snapshot.val());
+// });
 
-// Events
-var events_ref = db.ref("events");
-events_ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
+// // Events
+// var events_ref = db.ref("events");
+// events_ref.on("value", function(snapshot) {
+//   console.log(snapshot.val());
+// });
+
+// Notifications
+var notification_ref = db.ref("notifications").child('box');
+notification_ref.on("child_added", function(snapshot, prevChildKey) {
+	var newNotification = snapshot.val();
+	console.log("Message: " + newNotification.msg);
+	console.log("From: " + newNotification.from);
 });
 
 /* Instead of doing console.log to display the entire database, it can do notification, send email!  */
